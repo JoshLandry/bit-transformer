50c23792a6bde6af05e009941b7613ab9c7b28ff
Daniel Nofziger <dannofziger@gmail.com>
2015-02-05 19:27:07 +0000

    Fixed Gruntfile tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,31 @@
 'use strict';
 
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-simple-mocha');
-    
-    grunt.initConfig({
-        jshint: {
-            dev: {
-                options:{
-                    node: true,
-                    globals: {
-                        describe: true,
-                        it: true,
-                        before: true,
-                        after: true
-                    }
-                },
-                src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
-            }
-        },
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
 
-        simplemocha: {
-            all: {
-                src: ['test/**/*.js']
+  grunt.initConfig({
+    jshint: {
+        dev: {
+          options:{
+            node: true,
+            globals: {
+              describe: true,
+              it: true,
+              before: true,
+              after: true
             }
+          },
+        src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
         }
-    });
-    
-    grunt.registerTask('test', ['jshint:dev', 'simplemocha:all']);
-    grunt.registerTask('default', ['test']);
+    },
+    simplemocha: {
+      all: {
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['jshint:dev', 'simplemocha:all']);
+  grunt.registerTask('default', ['test']);
 };
