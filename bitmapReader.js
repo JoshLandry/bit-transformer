20ccf944fe25d410b881c5a7a2c6c5e99d6d885d
Josh <bluemazaro@yahoo.com>
2015-02-06 17:31:44 -0800

    randomized bitmap reader

--- bitmapReader.js
+++ bitmapReader.js
@@ -6,7 +6,7 @@ var bitmap = fs.readFileSync('./img/test.bmp');
 var colorPalette = bitmap.slice(54, 1078);
 
 for(var i=0; i<1024; i++) {
-	colorPalette[i] = colorPalette[i + 2];
+	colorPalette[i] = colorPalette[i + Math.floor(Math.random() * 10)];
 }
 
 fs.writeFileSync('newbmp.bmp', bitmap);
