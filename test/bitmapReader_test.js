65da4f7f613c965c1b54ab2854ac4c02c13dc6c4
Rory Sterley <royrsterley@gmail.com>
2015-02-08 16:41:21 -0800

    Add color palet test

--- test/bitmapReader_test.js
+++ test/bitmapReader_test.js
@@ -27,3 +27,29 @@ describe('bitmapReader', function() {
     expect(original).to.not.eql(newbmp);
   });
 });
+
+describe("bitmapReader", function() {
+  before(function() {
+    // remove ./img/newbmp.bmp if it exists.
+    try{
+      fs.unlinkSync('./img/newbmp.bmp');
+    } catch(err) {}
+  });
+
+  after(function() {
+    // remove ./img/newbmp.bmp if it exists.
+    try{
+      fs.unlinkSync('./img/newbmp.bmp');
+    } catch(err) {}
+  });
+
+  it("should modify the bitmap's color palot", function() {
+    var originalBmp = fs.readFileSync('./img/test.bmp');
+    var originalColorPalette = originalBmp.slice(54, 1078);
+    bitmapReader();
+    var newBmp = fs.readFileSync('./img/newbmp.bmp');
+    var newBmpColorPalette = newBmp.slice(54, 1078);
+
+    expect(originalColorPalette).not.to.eql(newBmpColorPalette);
+  });
+});
